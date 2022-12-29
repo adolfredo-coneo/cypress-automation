47430e8a855e3a79bf6d50d5475f6fbee488dab8
Adolfredo Coneo <ado.coneo@gmail.com>
2022-12-28 22:04:39 -0500

    add get element, type and assert

--- cypress/integrations/examples/Test1.spec.js
+++ cypress/integrations/examples/Test1.spec.js
@@ -1,5 +1,11 @@
+/// <reference types="Cypress" />
+
 describe('My First Test Suite', () => {
   it('My first test case!', () => {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+    cy.get('.search-keyword').type('ca');
+
+    cy.wait(2000);
+    cy.get('.product:visible').should('have.length', 4);
   });
 });
