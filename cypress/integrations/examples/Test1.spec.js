4b802ba990d632f844729818abec7d079275206d
Adolfredo Coneo <ado.coneo@gmail.com>
2022-12-29 23:55:27 -0500

    more cypress test utils

--- cypress/integrations/examples/Test1.spec.js
+++ cypress/integrations/examples/Test1.spec.js
@@ -6,6 +6,28 @@ describe('My First Test Suite', () => {
     cy.get('.search-keyword').type('ca');
 
     cy.wait(2000);
+    cy.get('.product').should('have.length', 5);
     cy.get('.product:visible').should('have.length', 4);
+
+    // Parent child chaining
+    cy.get('.products').find('.product').should('have.length', 4);
+
+    cy.get('.products')
+      .find('.product')
+      .eq(2)
+      .contains('ADD TO CART')
+      .click()
+      .then(() => {
+        console.log('sf');
+      });
+
+    cy.get('.products')
+      .find('.product')
+      .each(($el, index, $list) => {
+        const textVeg = $el.find('h4.product-name').text();
+        if (textVeg.includes('Cashews')) {
+          $el.find('button').trigger('click');
+        }
+      });
   });
 });
